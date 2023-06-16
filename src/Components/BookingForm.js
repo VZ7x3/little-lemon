602ecadc89af3503d3556f80f35f575b31769d3d
VZ7x3 <68671868+VZ7x3@users.noreply.github.com>
2023-06-16 15:04:39 -0700

    added form

---
+++ src/Components/BookingForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function BookingForm() {
+  return (
+    <form class = 'form' >
+    <label for="res-date">Choose date</label>
+    <input type="date" id="res-date"></input>
+    <label for="res-time">Choose time</label>
+    <select id="res-time ">
+       <option>17:00</option>
+       <option>18:00</option>
+       <option>19:00</option>
+       <option>20:00</option>
+       <option>21:00</option>
+       <option>22:00</option>
+    </select>
+    <label for="guests">Number of guests</label>
+    <input type="number" placeholder="1" min="1" max="10" id="guests"></input>
+    <label for="occasion">Occasion</label>
+    <select id="occasion">
+       <option>Birthday</option>
+       <option>Anniversary</option>
+    </select>
+    <div class = 'finishreservecontainer'>
+    <input type="submit" value="Make Your reservation" class = 'finishreserve'></input></div>
+ </form>
+  )
+}
--- src/Components/BookingPage.js
+++ src/Components/BookingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroBooking from './HeroBooking'
+import BookingForm from './BookingForm'
 export default function BookingPage() {
   return (
     <div>
   <main>
     <HeroBooking />
+    <BookingForm />
  </main>
     </div>
   )
